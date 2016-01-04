23ddd8f7f02e0fe3fec2aba5f9378801f03f5426
Priemsky <alexander.priemsky@nokia.com>
2016-01-04 17:37:18 +0300

    main function says goodbye

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
 #include "HelloWorld.hpp"
+#include "GoodbyeWorld.hpp"
 #include <memory>
 
 int main()
 {
 	std::unique_ptr<IGreeter> l_greeter = std::make_unique<HelloWorld>();
+	l_greeter->greet();
+	
+	l_greeter = std::make_unique<GoodbyeWorld>();
 	l_greeter->greet();
 	
 	return 0;
