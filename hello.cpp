5faf005d0df1c562f2c9e255830253dc504e62a6
Priemsky <alexander.priemsky@nokia.com>
2016-01-04 17:39:56 +0300

    add success and failure return codes

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,13 @@
 #include "HelloWorld.hpp"
 #include <memory>
 
+const int g_success = 0;
+const int g_failure = 1;
+
 int main()
 {
 	std::unique_ptr<IGreeter> l_greeter = std::make_unique<HelloWorld>();
 	l_greeter->greet();
 	
-	return 0;
+	return g_success;
 }
